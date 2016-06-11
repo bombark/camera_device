92e4080e0c587ea30853c46ca3d90bc3ac1059b1
Felipe Bombardelli <felipebombardelli@gmail.com>
2016-06-11 19:00:58 -0300

    Updated to new TiObj version

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <tiobj.hpp>
+#include <tiobj-cv.hpp>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -22,15 +23,18 @@ void sig_hnd(int sig){
 
 int main(){
 	TiObj params( getenv("params") );
-	int freq = params.atInt("freq");
+	int freq = params.atInt("freq", 10);
 	unsigned sleep_time = 1000000 / freq;
-	int dev_id = params.atInt("id");
+	int dev_id = params.atInt("id", 0);
 
 	G_capture.open( dev_id );
 
 	Mat img;
 	while(1){
+		TiObj obj;
 		G_capture >> img;
+		obj << img;
+		obj.save("image", true);
 
 		cout << "#end\n";
 		fflush(stdout);
